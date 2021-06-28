6127082ba51af7f00b2936e9ac4efaf25ecb10b6
hyun <coderhyun476@gmail.com>
2021-06-28 21:09:02 +0900

    reanimated

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,17 +1,43 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import FastImage from 'react-native-fast-image'
+import { StyleSheet, Text, View, Pressable, Button } from 'react-native'
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    withSpring,
+    withTiming,
+    cancelAnimation,
+} from 'react-native-reanimated';
+import { COLOR1, WIDTH } from '../../constants/styles';
+
 
 
 const HomeScreen = () => {
+    const offset = useSharedValue(0);
+
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: withSpring(offset.value * 255) }],
+        };
+    });
+
     return (
-        <View>
-            <Text>Home</Text>
-            <FastImage style={{ width: 200, height: 200, }} source={{ uri: 'https://github.com/DylanVann/react-native-fast-image/raw/master/docs/assets/scroll.gif' }} />
-        </View>
-    )
+        <>
+            <Animated.View style={[styles.box, animatedStyles]} />
+            <Button onPress={() => {
+                cancelAnimation(offset)
+                offset.value = Math.random()
+            }} title="Move" />
+        </>
+    );
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    box: {
+        width: 100,
+        height: 100,
+        marginTop: 100,
+        backgroundColor: COLOR1
+    }
+})
