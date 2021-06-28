07528ff39a92dc055910f699f59101c5ff30fd51
hyun <coderhyun476@gmail.com>
2021-06-28 18:43:53 +0900

    install fast image

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
+
 
 const HomeScreen = () => {
     return (
         <View>
             <Text>Home</Text>
+            <FastImage style={{ width: 200, height: 200, }} source={{ uri: 'https://github.com/DylanVann/react-native-fast-image/raw/master/docs/assets/scroll.gif' }} />
         </View>
     )
 }
