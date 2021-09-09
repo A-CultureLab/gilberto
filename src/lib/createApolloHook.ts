678c6193ae607a7ca325ce9eddbbe50caaf5ba02
hyun <coderhyun476@gmail.com>
2021-09-09 17:43:44 +0900

    Create Feature Error logger toast

--- src/lib/createApolloHook.ts
+++ src/lib/createApolloHook.ts
@@ -1,45 +1,56 @@
 import { MutationHookOptions, useMutation, QueryHookOptions, useQuery, ApolloError, useLazyQuery, SubscriptionHookOptions, useSubscription } from "@apollo/client"
-import { useNavigation } from "@react-navigation/core"
 import { DocumentNode } from "graphql"
+import useGlobalUi from "../hooks/useGlobalUi"
 
 
-const errorLogger = (error: ApolloError) => {
+const errorLogger = (error: ApolloError, toast: any) => {
     if (__DEV__) console.log(error.message)
-    // toastMessageVar(error.message)
+    for (const graphqlError of error.graphQLErrors) {
+        if (graphqlError.extensions?.notification) {
+            // toast
+            toast({ content: graphqlError.message })
+        } else {
+            toast({ content: '알 수 없는 오류' })
+        }
+    }
 }
 
 
 
-export const createQueryHook = <Data, Vars>(query: DocumentNode, preOptions?: QueryHookOptions<Data, Vars>) => (options?: QueryHookOptions<Data, Vars>) =>
-    useQuery<Data, Vars>(query, {
+export const createQueryHook = <Data, Vars>(query: DocumentNode, preOptions?: QueryHookOptions<Data, Vars>) => (options?: QueryHookOptions<Data, Vars>) => {
+    const { toast } = useGlobalUi()
+    return useQuery<Data, Vars>(query, {
         ...preOptions,
         ...options,
         onError: (error) => {
-            errorLogger(error)
+            errorLogger(error, toast)
             options?.onError && options.onError(error)
         },
     })
+}
 
-export const createLazyQueryHook = <Data, Vars>(query: DocumentNode, preOptions?: QueryHookOptions<Data, Vars>) => (options?: QueryHookOptions<Data, Vars>) =>
-    useLazyQuery<Data, Vars>(query, {
+export const createLazyQueryHook = <Data, Vars>(query: DocumentNode, preOptions?: QueryHookOptions<Data, Vars>) => (options?: QueryHookOptions<Data, Vars>) => {
+    const { toast } = useGlobalUi()
+    return useLazyQuery<Data, Vars>(query, {
         ...preOptions,
         ...options,
         onError: (error) => {
-            errorLogger(error)
+            errorLogger(error, toast)
             options?.onError && options.onError(error)
         },
     })
-
-export const createMutationHook = <Data, Vars>(query: DocumentNode, preOptions?: MutationHookOptions<Data, Vars>) => (options?: MutationHookOptions<Data, Vars>) =>
-    useMutation<Data, Vars>(query, {
+}
+export const createMutationHook = <Data, Vars>(query: DocumentNode, preOptions?: MutationHookOptions<Data, Vars>) => (options?: MutationHookOptions<Data, Vars>) => {
+    const { toast } = useGlobalUi()
+    return useMutation<Data, Vars>(query, {
         ...preOptions,
         ...options,
         onError: (error) => {
-            errorLogger(error)
+            errorLogger(error, toast)
             options?.onError && options.onError(error)
         },
     })
-
+}
 export const createSubscriptionHook = <Data, Vars>(query: DocumentNode, preOptions?: SubscriptionHookOptions<Data, Vars>) => (options?: SubscriptionHookOptions<Data, Vars>) =>
     useSubscription<Data, Vars>(query, {
         ...preOptions,
